3a181fd89215a37359d9039a66bf12346b15fd5e
Chris Kim <chrisk8599@gmail.com>
2025-11-24 14:27:34 +1100

    try again 2

--- backend/lib/models/vendor.py
+++ backend/lib/models/vendor.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, ForeignKey,ARRAY  
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from lib.database import Base
